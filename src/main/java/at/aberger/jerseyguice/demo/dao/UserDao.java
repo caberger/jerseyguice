2d3271d265a752242aa871de45c22ed8d5983c44
Christian Aberger <aberger.christian@gmail.com>
2016-11-25 08:56:21 +0100

    removed duplicate demo package

--- src/main/java/at/aberger/jerseyguice/demo/dao/UserDao.java
+++
@@ -1,6 +0,0 @@
-package at.aberger.jerseyguice.demo.dao;
-
-import at.aberger.jerseyguice.demo.model.User;
-
-public abstract class UserDao implements DataAccessObject<User> {
-}
--- src/main/java/at/aberger/jerseyguice/demo/model/User.java
+++
@@ -1,7 +0,0 @@
-package at.aberger.jerseyguice.demo.model;
-
-public class User {
-	public int id;
-	public String name;
-	public String email;
-}
